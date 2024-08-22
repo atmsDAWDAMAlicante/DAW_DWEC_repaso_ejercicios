6c5c56615b07bdecda4dc165aa76ee2213287e48
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-22 20:27:25 +0200

    Implementación de la multiplicación y la división

--- Tema_01_Introduccion_al_desarrollo_web_en_entorno_cliente/DWEC_U01_A03_CPE_ARCHIVOS/operations.js
+++ Tema_01_Introduccion_al_desarrollo_web_en_entorno_cliente/DWEC_U01_A03_CPE_ARCHIVOS/operations.js
@@ -1,22 +1,25 @@
 /* Función suma */
 function suma(a, b) {
+      // esta operación suma es correcta y pasa el test
 	  return a + b;
 };
 
 /* Función resta */
 function resta(a, b) {
+      // esta operación resta es correcta y pasa el test
 	  return a - b;
 };
 
 function multiplicacion(a, b) {
-    /* TODO */
+    // La función aparece vacía al comienzo del ejercicio; al implementarla correctamente pasa el test
+    return a * b;
 
 }
 
 /* Función división */
 function division(a, b) {
-    /* TODO */
-
+    // La función aparece vacía al comienzo del ejercicio; al implementarla correctamente pasa el test
+    return a / b;
 }
 
 /* Exportación de funciones */
