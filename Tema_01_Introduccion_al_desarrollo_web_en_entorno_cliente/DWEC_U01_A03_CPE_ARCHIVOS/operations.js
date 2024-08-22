7c42af54740cefbf130d34571557cc5429cd1627
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-08-22 20:23:15 +0200

    DWEC_U01_A03_CPE_ARCHIVOS

---
+++ Tema_01_Introduccion_al_desarrollo_web_en_entorno_cliente/DWEC_U01_A03_CPE_ARCHIVOS/operations.js
@@ -0,0 +1,28 @@
+/* Función suma */
+function suma(a, b) {
+	  return a + b;
+};
+
+/* Función resta */
+function resta(a, b) {
+	  return a - b;
+};
+
+function multiplicacion(a, b) {
+    /* TODO */
+
+}
+
+/* Función división */
+function division(a, b) {
+    /* TODO */
+
+}
+
+/* Exportación de funciones */
+module.exports = {
+	  suma,
+	  resta,
+    multiplicacion,
+    division
+}
