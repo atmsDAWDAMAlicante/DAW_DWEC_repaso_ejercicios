c3e99768fbb6dbd77624c0c8055b86ec4a90770c
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-13 11:17:15 +0200

    Programa y test completos

--- Tema_02_Javascript/Ejercicios_Tema_04_Programacion/ejercicios/Prog_Tema_4_Ej_02.js
+++ Tema_02_Javascript/Ejercicios_Tema_04_Programacion/ejercicios/Prog_Tema_4_Ej_02.js
@@ -1,8 +1,19 @@
 /*  Ejercicio nº 2 del Tema 4 de la asignatura Programación del ciclo DAW, Curso 2022-2023
-Enunciado: Realizar un programa que lea 2 números y compruebe si el primero de esos números es divisible por el otro. Hacer con una función que devolverá verdadero o falso.
+Enunciado: Realizar un programa que lea 2 números y compruebe si el primero de esos números es divisible por el otro. 
+Hacer con una función que devolverá verdadero o falso.
 */
 
+let EsDivisible = function(a,b){
+    if ( (a%b) == 0){
+        return true;
+    } else {
+        return false;
+    }
+};
 
+console.log(`¿4 es divisible por 4? Respuesta: ${EsDivisible(4,4)}`);
+console.log(`¿7 es divisible por 3? Respuesta: ${EsDivisible(7,3)}`);
 
-
-
+module.exports = {
+    EsDivisible
+}
