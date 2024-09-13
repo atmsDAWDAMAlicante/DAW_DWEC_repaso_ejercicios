b0f0df12f5729cf73c9a9ee36425930b02e94cdf
Angel Tomás <118595811+atmsDAWDAMAlicante@users.noreply.github.com>
2024-09-13 11:21:21 +0200

    Test completo

--- Tema_02_Javascript/Ejercicios_Tema_04_Programacion/test/Test_Tema_4_Ej_01.js
+++ Tema_02_Javascript/Ejercicios_Tema_04_Programacion/test/Test_Tema_4_Ej_01.js
@@ -3,3 +3,18 @@ let should = require('should');
 
 // Carga del módulo con las funciones a probar
 let ejercicios = require('../ejercicios/Prog_Tema_4_Ej_01.js');
+
+it ("Función tradicional: suma1()", function(){
+    ejercicios.suma1.should.be.a.Function; // comprobación que suma1() es una función
+    should.equal(ejercicios.suma1(2,2), 4, "mensaje en caso de no pasar la prueba");
+});
+
+it("Función en una variable: suma2()", function(){
+    ejercicios.suma2.should.be.a.Function;
+    should.equal(ejercicios.suma2(6,7), 13, "No coincide con el resultado");
+});
+
+it("Función flecha: suma3()", function(){
+    ejercicios.suma3.should.be.a.Function;
+    should.equal(ejercicios.suma3(9,9), 18, "La suma no está bien hecha");
+});
--- Tema_02_Javascript/Ejercicios_Tema_04_Programacion/test/Test_Tema_4_Ej_02.js
+++ Tema_02_Javascript/Ejercicios_Tema_04_Programacion/test/Test_Tema_4_Ej_02.js
@@ -3,3 +3,9 @@ let should = require('should');
 
 // Carga del módulo con las funciones a probar
 let ejercicios = require('../ejercicios/Prog_Tema_4_Ej_02.js');
+
+it("Función EsDivisible()", function(){
+    ejercicios.EsDivisible.should.be.a.Function;
+    (ejercicios.EsDivisible(2,2)).should.be.true("Ha fallado la prueba 2%2");
+    (ejercicios.EsDivisible(3,2)).should.not.be.true("Ha fallado la prueba de 3%2==false");
+});
