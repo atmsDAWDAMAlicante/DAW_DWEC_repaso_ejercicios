150cf2e50347afed96beda3ed636a775828b9a5a
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-13 14:16:50 +0200

    Programa y test completos

--- Tema_02_Javascript/Ejercicios_Tema_04_Programacion/ejercicios/Prog_Tema_4_Ej_03.js
+++ Tema_02_Javascript/Ejercicios_Tema_04_Programacion/ejercicios/Prog_Tema_4_Ej_03.js
@@ -2,7 +2,32 @@
 Enunciado: Realizar un subprograma que intercambie el valor de 2 variables enteras y visualice los valores intercambiados.
 */
 
+/* NOTA: el Tema 4 "Programación Modular" de la asignatura de 'Programación' (DAW) se explica antes que los arrays */
+
+let Intercambio = function(a,b){
+    console.log(`1ª posición original: ${a}`)
+    console.log(`2ª posición original: ${b}`)
+    let cambio = [];
+    cambio.push(b);
+    cambio.push(a);
+    return cambio;
+}
 
 
+let resultado = Intercambio(1,2);
+
+
+console.log("--------------------");
+
+
+for (i = 0; i < resultado.length; i++)
+{
+    console.log(`${(i+1)}ª posición cambiada: ${resultado[i]}`)
+}
+
+
+module.exports = {
+    Intercambio
+}
 
 
--- Tema_02_Javascript/Ejercicios_Tema_04_Programacion/test/Test_Tema_4_Ej_03.js
+++ Tema_02_Javascript/Ejercicios_Tema_04_Programacion/test/Test_Tema_4_Ej_03.js
@@ -3,3 +3,10 @@ let should = require('should');
 
 // Carga del módulo con las funciones a probar
 let ejercicios = require('../ejercicios/Prog_Tema_4_Ej_03.js');
+
+it("Función Intercambio()", function(){
+    ejercicios.Intercambio.should.be.a.Function;
+    should.deepEqual(ejercicios.Intercambio(3,4), [4,3]);
+    // equal con un Array falla
+    //should.equal(ejercicios.Intercambio(9,100), [100,9]); 
+})
