255e212cbb603b825b49fc2deebd50546f9b82e8
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-13 10:54:01 +0200

    Programa y test completos

--- Tema_02_Javascript/Ejercicios_Tema_04_Programacion/ejercicios/Prog_Tema_4_Ej_01.js
+++ Tema_02_Javascript/Ejercicios_Tema_04_Programacion/ejercicios/Prog_Tema_4_Ej_01.js
@@ -2,5 +2,24 @@
 Enunciado: Realizar un programa que lea dos números y los sume con una función que realice la suma.
 */
 
+function suma1(a,b){
+    return a+b;
+}
+
+let suma2 = function(a,b){
+    return a+b;
+}
+
+let suma3 = (a,b)=>a+b;
 
 
+console.log(`Función suma1() de 2 + 3 = ${suma1(2,3)}`);
+console.log(`Función suma2() de 4 + 5 = ${suma2(4,5)}`);
+console.log(`Funcion suma3() de 1 + 9 = ${suma3(1,9)}`);
+
+
+module.exports = {
+    suma1,
+    suma2,
+    suma3
+}
