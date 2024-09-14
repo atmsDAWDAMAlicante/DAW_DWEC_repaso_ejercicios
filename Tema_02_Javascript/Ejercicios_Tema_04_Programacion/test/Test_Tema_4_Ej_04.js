d6d2a62dc3dc768bd64090a0ee8e5689288b5723
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-14 14:17:12 +0200

    Ejercicio 4-Tema 4 y prueba unitaria completos

--- Tema_02_Javascript/Ejercicios_Tema_04_Programacion/test/Test_Tema_4_Ej_04.js
+++ Tema_02_Javascript/Ejercicios_Tema_04_Programacion/test/Test_Tema_4_Ej_04.js
@@ -3,3 +3,9 @@ let should = require('should');
 
 // Carga del módulo con las funciones a probar
 let ejercicios = require('../ejercicios/Prog_Tema_4_Ej_04.js');
+
+it ("Función: ValorAbsoluto()", function(){
+    ejercicios.ValorAbsoluto.should.be.a.Function;
+    should.equal(ejercicios.ValorAbsoluto(-4),4);
+    (ejercicios.ValorAbsoluto(-5)).should.be.exactly(5).and.be.a.Number();
+});
