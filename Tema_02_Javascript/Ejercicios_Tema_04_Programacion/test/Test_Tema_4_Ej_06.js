74b0da23f8b28654d1b21d27da9e0b8f7fdbb71e
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-15 17:18:20 +0200

    Diseño del test sin probar aún

--- Tema_02_Javascript/Ejercicios_Tema_04_Programacion/test/Test_Tema_4_Ej_06.js
+++ Tema_02_Javascript/Ejercicios_Tema_04_Programacion/test/Test_Tema_4_Ej_06.js
@@ -3,3 +3,35 @@ let should = require('should');
 
 // Carga del módulo con las funciones a probar
 let ejercicios = require('../ejercicios/Prog_Tema_4_Ej_06.js');
+
+// Hay que probar dos funciones, una 'Division' devuelve la división de dos enteros y otra 'Resto' devuelve el resto
+
+let arrayDivision = [
+    [4,2,2],
+    [5,3,1],
+    [16,20,0]
+]
+
+let arrayResto = [
+    [4,2,0],
+    [5,3,2],
+    [16,20,16]
+]
+
+
+describe("Prueba de la division entera:", function(){
+    arrayDivision.forEach(([input1, input2, expected], index)=>{
+        it(`División: supuesto nº ${index}: `, function(){
+            (ejercicios.Division(input1, input2)).should.be.exactly(expected).and.be.a.Number();
+        })
+    });
+});
+
+
+describe("Prueba del resto entero:", function(){
+    arrayResto.forEach(([input1, input2, expected], index)=>{
+        it(`Resto: supuesto nº ${index}: `, function(){
+            (ejercicios.Resto(input1, input2)).should.be.exactly(expected).and.be.a.Number();
+        });
+    })
+});
