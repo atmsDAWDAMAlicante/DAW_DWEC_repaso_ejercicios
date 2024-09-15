831acb643aa961bfe1ff7ea6dd3e9ccaf96f2120
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-15 19:39:09 +0200

    Ejercicio nº 6 (tema 4) y test completos

--- Tema_02_Javascript/Ejercicios_Tema_04_Programacion/ejercicios/Prog_Tema_4_Ej_06.js
+++ Tema_02_Javascript/Ejercicios_Tema_04_Programacion/ejercicios/Prog_Tema_4_Ej_06.js
@@ -4,17 +4,29 @@ Enunciado: Realiza un programa con un subprograma al que se le pasen dos número
 
 // Hay que crear dos funciones: una 'Division' devuelve la división entera de dos números y otra 'Resto' devuelve el resto.
 
+// 1º Las dos funciones pasan los tests
+
 let Division = function(a, b){
-    parseInt(a);
-    parseInt(b);
-    return a/b;
+    return parseInt(a/b);
 };
-let Resto = (a,b)=>a%b;
-/*
-function Resto(a,b){
-    return a%b;
+
+let Resto = (a,b)=> a%b;
+
+// 2º Función a la que se le pasan las dos funciones anteriores como callbacks
+
+function ResultadoCompleto(a, b, callbackDivision, callbackResto){
+    console.log(`El resultado de la división entera de ${a}/${b} = ${callbackDivision(a,b)}`);
+    console.log(`El resultado del módulo de ${a}%${b} = ${callbackResto(a,b)}`);
 }
-*/
+
+// Prueba de la función con los parámetros correctos
+console.log("1º Parámetros correctos");
+ResultadoCompleto(4,2,Division,Resto);
+
+//Prueba de la función con un parámetro que hace otra cosa
+let Suma = (a,b)=>a+b;
+console.log("2º Un parámetro incorrecto");
+ResultadoCompleto(5,3,Division,Suma);
 
 
 module.exports = {
