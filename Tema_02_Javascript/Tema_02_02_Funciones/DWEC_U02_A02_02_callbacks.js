110af08aeb23e8f7a2d6f80cf19150a13c5a99a2
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-15 17:03:08 +0200

    Teoría: callbacks

--- Tema_02_Javascript/Tema_02_02_Funciones/DWEC_U02_A02_02_callbacks.js
+++ Tema_02_Javascript/Tema_02_02_Funciones/DWEC_U02_A02_02_callbacks.js
@@ -1,3 +1,5 @@
+// A) CÓDIGO ORIGINAL PARA EJECUTAR EN EL NAVEGADOR
+/*
 function confirmar(pregunta, si, no) {
     if (confirm(pregunta))
 	si(); // Se ejecuta la función de callback pasada en el parámetro 'si'
@@ -25,3 +27,32 @@ function aceptar2() {
 }
 
 confirmar("¿Sabes qué es un callback?", aceptar2, cancelar);
+*/
+
+// B) ADAPTACIÓN PARA EJECUTAR EL CÓDIGO EN NODE.JS
+
+function confirmar(pregunta, si, no){
+    if(pregunta){ // pregunta se pasa como un valor booleano, en el código original se ejecuta la función confirm del navegador
+        si(); // Se ejecuta la función si() que es la función aceptar() pasada por parámetro
+    }
+    else{
+        no(); // Se ejecuta la función no() que es la función cancelar() pasada por parámetro
+    }
+}
+
+function aceptar(){
+    console.log("Has elegido aceptar");
+}
+function cancelar(){
+    console.log("Has elegido cancelar");
+}
+
+// llamada a la función confirmar
+// 1º le paso true
+confirmar(true, aceptar, cancelar); // se les pasa el nombre de la función sin paréntesis
+// ese 'aceptar' y 'cancelar' son los callbacks
+// 2º le paso false
+confirmar(false, aceptar, cancelar);
+
+// Si se pone aceptar() lo que se estaría pasando por parámetro es el resultado de la función y no la función, y en este caso daría error.
+// confirmar(true,aceptar(),cancelar()); TypeError: si is not a function
