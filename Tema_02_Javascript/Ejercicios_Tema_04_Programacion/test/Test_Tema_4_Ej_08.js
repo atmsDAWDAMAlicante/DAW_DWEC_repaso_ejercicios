76dbcd1a5d8e71fef32c82c63c495f43f1bb63ec
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-16 19:20:49 +0200

    Función ValidarNumerosIntroducidos y su test

--- Tema_02_Javascript/Ejercicios_Tema_04_Programacion/test/Test_Tema_4_Ej_08.js
+++ Tema_02_Javascript/Ejercicios_Tema_04_Programacion/test/Test_Tema_4_Ej_08.js
@@ -3,3 +3,27 @@ let should = require('should');
 
 // Carga del módulo con las funciones a probar
 let ejercicios = require('../ejercicios/Prog_Tema_4_Ej_08.js');
+
+let arrayNotasMedias = [
+    [5,5,5,5],
+    [3,3,3,3],
+    [0,0,0,0],
+]
+
+let arrayNumerosValidos = [
+    [1,30,40,false],
+    [50,60,1,false],
+    [70,1,-1,false],
+    [0,0,0,true],
+    [4,6,8,true]
+];
+
+
+describe("Test valores introducidos son válidos", function(){
+    arrayNumerosValidos.forEach(([input1,input2,input3,expected], index) => {
+        it(`El test nº ${index} con los valores: ${input1},${input2},${input3}`, function(){
+            ejercicios.ValidarNumerosIntroducidos.should.be.a.Function;
+            ejercicios.ValidarNumerosIntroducidos(input1, input2, input3).should.be.exactly(expected);
+        });
+    });
+});
