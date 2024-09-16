5ced1c71e75d71fadf72022fea970bab1d034945
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-09-16 20:29:07 +0200

    Ejercicio nº 8 (tema 4) y test completos

--- Tema_02_Javascript/Ejercicios_Tema_04_Programacion/ejercicios/Prog_Tema_4_Ej_08.js
+++ Tema_02_Javascript/Ejercicios_Tema_04_Programacion/ejercicios/Prog_Tema_4_Ej_08.js
@@ -18,12 +18,26 @@ function ValidarNumerosIntroducidos(...notas){
 
 
 function CalcularMedia (...notas){
-
+    let total = 0;
+    let longitud = notas.length;
+    let validarNotas = ValidarNumerosIntroducidos(...notas);
+    if (validarNotas){
+        for(let nota of notas){
+            total += nota;
+        }
+        // IMPORTANTE: aquí se devuelve la media.
+        // Los decimales se separan con puntos. 
+        // Sólo dos posiciones decimales se obtienen con el método .toFixed(2) que devuelve un string
+        // El string hay que convertirlo en float con parseFloat()
+        return parseFloat((total/longitud).toFixed(2));
+    } else {
+        return -1;
+    }
 }
 
-console.log(ValidarNumerosIntroducidos(1,20,30))
-
-
+console.log(ValidarNumerosIntroducidos(1,20,30)); // false
+console.log(CalcularMedia(2,3,4,57,5,4,3)); // -1 
+console.log(CalcularMedia(2,3,4,5,5,4,3)); // 3.71
 
 module.exports = {
     ValidarNumerosIntroducidos,
--- Tema_02_Javascript/Ejercicios_Tema_04_Programacion/test/Test_Tema_4_Ej_08.js
+++ Tema_02_Javascript/Ejercicios_Tema_04_Programacion/test/Test_Tema_4_Ej_08.js
@@ -4,12 +4,6 @@ let should = require('should');
 // Carga del módulo con las funciones a probar
 let ejercicios = require('../ejercicios/Prog_Tema_4_Ej_08.js');
 
-let arrayNotasMedias = [
-    [5,5,5,5],
-    [3,3,3,3],
-    [0,0,0,0],
-]
-
 let arrayNumerosValidos = [
     [1,30,40,false],
     [50,60,1,false],
@@ -18,12 +12,40 @@ let arrayNumerosValidos = [
     [4,6,8,true]
 ];
 
-
+// Test para validar la función que valida que los números introducidos están entre 0 y 10
 describe("Test valores introducidos son válidos", function(){
     arrayNumerosValidos.forEach(([input1,input2,input3,expected], index) => {
         it(`El test nº ${index} con los valores: ${input1},${input2},${input3}`, function(){
             ejercicios.ValidarNumerosIntroducidos.should.be.a.Function;
-            ejercicios.ValidarNumerosIntroducidos(input1, input2, input3).should.be.exactly(expected);
+            ejercicios.ValidarNumerosIntroducidos(input1, input2, input3).should.be.exactly(expected).and.be.a.Boolean;
+        });
+    });
+});
+
+
+// Test que valida la función que calcula la media
+
+// IMPORTANTE:
+// Los decimales se separan con puntos. 
+// Sólo dos posiciones decimales se obtienen con el método .toFixed(2) que devuelve un string
+// El string hay que convertirlo en float con parseFloat()
+
+let arrayNotasMedias = [
+    [5,5,5,5],
+    [7,2,5,4.67],
+    [0,0,0,0],
+    [7,9,4,6.67],
+    [3,5,8,5.33],
+    [0,40,0,-1]
+]
+
+
+
+describe("Test cálculo de la media", function(){
+    arrayNotasMedias.forEach(([input1, input2, input3, expected], index)=>{
+        it(`El test nº ${index} con las notas: ${input1},${input2},${input3}`, function(){
+            ejercicios.CalcularMedia.should.be.a.Function;
+            ejercicios.CalcularMedia(input1,input2,input3).should.be.exactly(expected).and.be.a.Number;
         });
     });
 });
