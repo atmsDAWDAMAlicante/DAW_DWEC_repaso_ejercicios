40f1a66bf3ff8786b359174fc309332e5c86dfde
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-02-08 19:04:55 +0100

    Creado proyecto con El_Componente

---
+++ Tema_06_Desarrollo_con_Vue_js/Tema_06_02_Componentes_en_VUE/index4/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import EL_COMPONENTE from './components/EL_COMPONENTE.vue';
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+<div>Hola mundo</div>
+<div>
+  <HelloWorld></HelloWorld>
+  <EL_COMPONENTE></EL_COMPONENTE>
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Tema_06_Desarrollo_con_Vue_js/Tema_06_02_Componentes_en_VUE/index4/src/components/EL_COMPONENTE.vue
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+
+<template>
+    <div>
+   <p>
+<span class="marcadorojo"> hola soy el componente</span>
+   </p>
+</div>
+</template>
+
+<style>
+.marcadorojo{color:#FF0000;}
+</style>
