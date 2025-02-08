471c0de0ffdd229df0d2ec54366463abb34729b9
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-02-08 18:18:08 +0100

    Comenzando el punto 2 Componentes en Vue

---
+++ Tema_06_Desarrollo_con_Vue_js/Tema_06_02_Componentes_en_VUE/prueba1.vue
@@ -0,0 +1,33 @@
+<template>
+
+
+</template>
+
+<script>
+// no se hace la app como en appCreate
+// hay que exportar
+// export default {OBJETO}
+
+export default {
+    name: "NombreComponente",
+    data(){
+
+    },
+    methods:{
+
+    },
+    computed:{
+        
+    }
+}
+
+
+
+
+
+</script>
+
+
+
+<style>
+</style>
