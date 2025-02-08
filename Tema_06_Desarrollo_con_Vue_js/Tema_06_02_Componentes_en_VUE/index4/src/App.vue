2c6bcdf9b3f2610f3b0ab3914894acd5762bc563
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-02-08 20:11:36 +0100

    Hooks y props - minuto 45

--- Tema_06_Desarrollo_con_Vue_js/Tema_06_02_Componentes_en_VUE/index4/src/App.vue
+++ Tema_06_Desarrollo_con_Vue_js/Tema_06_02_Componentes_en_VUE/index4/src/App.vue
@@ -1,7 +1,17 @@
-<script setup>
+<!--<script setup>-->
+<script>
+
 import HelloWorld from './components/HelloWorld.vue'
 import EL_COMPONENTE from './components/EL_COMPONENTE.vue';
 import TheWelcome from './components/TheWelcome.vue'
+
+export default {
+    components: {
+        HelloWorld,
+        EL_COMPONENTE
+    }
+}
+
 </script>
 
 <template>
--- Tema_06_Desarrollo_con_Vue_js/Tema_06_02_Componentes_en_VUE/index4/src/components/EL_COMPONENTE.vue
+++ Tema_06_Desarrollo_con_Vue_js/Tema_06_02_Componentes_en_VUE/index4/src/components/EL_COMPONENTE.vue
@@ -1,16 +1,32 @@
 <script>
 
+
+export default {
+    name: "elComponente",
+    data(){
+        return{
+            saludo1: "Hola soy tu amigo el componente",
+            unArray: [1,3,4,56,4,2345,25,4254,56423,54,452345,2],
+            verde:true
+        }
+    }
+
+}
 </script>
 
 
 <template>
     <div>
    <p>
-<span class="marcadorojo"> hola soy el componente</span>
+<span class="marcadoazul"> {{ saludo1 }}</span>
    </p>
+   <ul>
+    <li v-for="numero in unArray">{{ numero }}</li>
+   </ul>
 </div>
 </template>
 
 <style>
 .marcadorojo{color:#FF0000;}
+.marcadoazul{color:#0000FF;}
 </style>
