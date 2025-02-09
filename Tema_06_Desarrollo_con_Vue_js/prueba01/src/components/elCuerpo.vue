cb3e65d66e4fe40a83fbce6dc12654db15c13be4
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-02-09 19:25:54 +0100

    prueba01

---
+++ Tema_06_Desarrollo_con_Vue_js/prueba01/src/components/elCuerpo.vue
@@ -0,0 +1,30 @@
+<script>
+export default{
+    name:"componenteHijo",
+    data(){
+        return{
+            argo:""
+        }
+    },
+    props:[
+        "variableArray"
+    ]
+}
+</script>
+
+
+<template>
+    <div>
+        <ul>
+            <li v-for="elemento in variableArray">{{ elemento.nombre }}</li>
+        </ul>
+        <pre>{{ variableArray }}</pre>
+    </div>
+</template>
+
+<style>
+li{
+    color:aquamarine
+}
+pre{color:white}
+</style>
---
+++ Tema_06_Desarrollo_con_Vue_js/prueba01/src/components/elHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: "elHeaderDeArriba",
+    data(){
+        return{
+            elTitulo: "HOLA SOY TU AMIGO EL COMPONENTE"
+        }
+    }
+}
+</script>
+
+<template>
+    <header><h1>{{ elTitulo }}</h1></header>
+</template>
+
+<style>
+header{
+  border: 2px solid #0000FF; 
+  color: red;
+  margin:10px;
+  height: auto;
+  width: 90%;
+}
+</style>
