6be51b4f4b74fac7ceb6aaee35866e494d8e67e8
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-02-22 19:50:50 +0100

    Primeros pasos y verificación props

--- Tema_06_Desarrollo_con_Vue_js/Practica_Vue/prueba01/src/App.vue
+++ Tema_06_Desarrollo_con_Vue_js/Practica_Vue/prueba01/src/App.vue
@@ -1,47 +1,38 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+//import TheWelcome from './components/TheWelcome.vue'
+import GitHub from './components/GitHub.vue';
+export default{
+  components:{
+    GitHub
+  },
+  data(){
+    return{
+      algo: "Esto es algo"
+    }
+  }
+}
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <!--<img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />-->
+<GitHub :unProp="algo"></GitHub>
+    <!--<div class="wrapper">-->
+      <!--<HelloWorld msg="You did it!" />-->
+    <!--</div>-->
   </header>
 
   <main>
-    <TheWelcome />
+    <!--<TheWelcome />-->
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
+/*header {line-height: 1.5;}
+.logo {display: block;margin: 0 auto 2rem;}
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+  header {display: flex;place-items: center;padding-right: calc(var(--section-gap) / 2);}
+  .logo {margin: 0 2rem 0 0;}
+  header .wrapper {display: flex;place-items: flex-start;flex-wrap: wrap;}}*/
 </style>
---
+++ Tema_06_Desarrollo_con_Vue_js/Practica_Vue/prueba01/src/components/GitHub.vue
@@ -0,0 +1,27 @@
+<script>
+
+export default {
+    name:"componenteCabecera",
+    data(){
+        
+        return{
+            prueba:"Hola desde la cabecera"
+        }
+    },
+    props:[
+        "unProp"
+    ]
+}
+
+</script>
+
+<template>
+<header>{{prueba}}</header>
+<p>{{unProp}}</p>
+
+
+</template>
+
+<style>
+header, p{color:black;}
+</style>
