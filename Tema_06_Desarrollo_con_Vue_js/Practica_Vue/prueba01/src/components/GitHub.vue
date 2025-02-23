4f92b7f6f6d123810f1533f0ce4327575cf52244
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-02-23 17:58:36 +0100

    Se obtienen los valores del usuario de GitHub

--- Tema_06_Desarrollo_con_Vue_js/Practica_Vue/prueba01/src/components/GitHub.vue
+++ Tema_06_Desarrollo_con_Vue_js/Practica_Vue/prueba01/src/components/GitHub.vue
@@ -5,10 +5,16 @@ export default {
     data(){
         
         return{
+            // URL
             urlCompleta: "",
             urlParteFija: "https://api.github.com/users/",
             urlUsuario: "",
-            urlRepos:"/repos",
+            // GET
+            login:"",
+            avatar_url:"",
+            html_url:"",
+            repos_url:"",
+            // Booleanos
             mostrar: false,
             primeraVez: true
             
@@ -40,7 +46,13 @@ export default {
                     return response.json();
                 }
             }).then((data)=>{
+                this.login=data.login;
+                this.avatar_url=data.avatar_url;
+                this.html_url=data.html_url;
+                this.repos_url=data.repos_url;
 
+            }).catch((error)=>{
+                console.log(`Error de conexión: error ${response.status}`)
             })
 
         }
@@ -51,20 +63,31 @@ export default {
 
 <template>
 <main>
-    <input type="text" v-model="urlUsuario" @keydown.enter="llamadaGitHub">
-    <button @click="borrarConsola">Borrar consola</button>
-    <div><p>{{unProp}}</p>
-    <p>{{ urlParteFija }}{{ urlUsuario }}{{ urlRepos }}</p>
+    <div>
+        <input type="text" v-model="urlUsuario" @keydown.enter="llamadaGitHub">
+        <button @click="borrarConsola">Borrar consola</button> Búsqueda: {{ urlParteFija }}{{ urlUsuario }}
     </div>
+    
     <template v-if="primeraVez == false">
-    <div v-if="mostrar == true" class="usuarioExistente">
-        El usuario {{ urlUsuario }} SÍ existe.
+        <div v-if="mostrar == true" class="usuarioExistente">
+            <dl><dt>El usuario {{ urlUsuario }} SÍ existe:</dt>
+                <dd>Usuario: <span class="marcadorojo">{{ login }}</span></dd>
+                
+                <dd>HTML URL: <a :href="html_url" target="_blank"><span class="marcadoazul">{{ html_url }}</span></a></dd>
+                <dd>Repositorios: <a :href="repos_url" target="_blank"><span class="marcadoverde">{{ repos_url }}</span></a></dd>
+            </dl>
+            <figure><img :src="avatar_url" alt="Avatar">
+                <figcaption>Url del avatar: <a :href="avatar_url" target="_blank"><span class="marcadoverde">{{ avatar_url }}</span></a></figcaption>
+            </figure>
 
-    </div>
-    <div v-else class="usuarioInexistente">
-        El usuario {{ urlUsuario }} NO existe.
-    </div>
-</template>
+        </div>
+
+        <div v-else class="usuarioInexistente">
+            El usuario {{ urlUsuario }} NO existe.
+        </div>
+
+
+    </template>
   </main>
 </template>
 
@@ -81,8 +104,12 @@ header, p{color:black;}
 .usuarioExistente{
     border: 1px solid blue;
     background-color: rgb(229, 251, 255);
-    color: blue;
     font-weight: bold;
 }
+
+.marcadorojo{color:red}
+.marcadoverde{color:#339900}
+.marcadoazul{color:blue}
+.marcadomorado{color:purple}
 
 </style>
