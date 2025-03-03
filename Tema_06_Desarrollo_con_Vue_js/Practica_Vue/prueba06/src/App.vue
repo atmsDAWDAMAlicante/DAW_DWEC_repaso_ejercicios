52a404310b5072fb3d3ed51187af33fd0dbb6bf8
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-03-03 16:19:14 +0100

    Avanzado el display en el componente principal

--- Tema_06_Desarrollo_con_Vue_js/Practica_Vue/prueba06/src/App.vue
+++ Tema_06_Desarrollo_con_Vue_js/Practica_Vue/prueba06/src/App.vue
@@ -1,8 +1,75 @@
-<script setup>
+<script>
+import SeguidoresGitHub from './components/SeguidoresGitHub.vue';
+
+export default {
+  name: "x",
+  components:{
+    SeguidoresGitHub
+  }, data(){
+    return{
+      titulo: "Buscar seguidores de GitHub",
+
+      // Variables de display
+      primeraVez:true,
+      usuarioExistente:false,
+
+      // Variables de GitHub
+      usuario:"",
+      urlBasica:"https://api.github.com/users/",
+      urlCompleta:""
+    }
+  },
+  methods:{
+    borrarConsola(){
+      console.clear();
+    },
+    borrarInput(){
+      this.usuario="";
+    },
+    obtenerUsuario(){
+      console.log(this.usuario);
+      this.primeraVez = false;
+    }
+  }
+}
+
 
 </script>
 
 <template>
+
+<header class="containter">
+  <div class="row">
+    <div class="col col-1"></div>
+    <div class="alert alert-warning col col-8" role="alert">
+      <h1>{{ titulo }}</h1>
+    </div>
+  </div>
+</header>
+
+<nav class="containter">
+  <div class="row">
+    <div class="col col-1"></div>
+    <div class="col col-6">
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1" v-model="usuario"
+        @keydown.enter="obtenerUsuario"> 
+      </div>
+    </div>
+    <div class="col col-1">
+      <button class="btn btn-primary" @click="borrarConsola">#</button>
+    </div>
+    <div class="col col-1">
+      <button class="btn btn-success" @click="borrarInput">#</button>
+    </div>
+  </div>
+</nav>
+<div>&nbsp;</div><div>&nbsp;</div><div>&nbsp;</div>
+
+<main v-if="primeraVez == false" class="containter">
+  <SeguidoresGitHub></SeguidoresGitHub>
+</main>
+
 
 </template>
 
---
+++ Tema_06_Desarrollo_con_Vue_js/Practica_Vue/prueba06/src/components/SeguidoresGitHub.vue
@@ -0,0 +1,36 @@
+<script>
+
+export default {
+  name: "ComponenteSeguidores",
+  components:{
+
+  }, data(){
+    return{
+      titulo: "Seguidores GitHub"
+    }
+  },
+  methods:{
+
+  }
+}
+
+
+</script>
+
+<template>
+
+<header>
+  <h1>{{ titulo }}</h1>
+</header>
+
+
+<main>
+
+</main>
+
+
+</template>
+
+<style scoped>
+  h1{color:yellow;}
+</style>
