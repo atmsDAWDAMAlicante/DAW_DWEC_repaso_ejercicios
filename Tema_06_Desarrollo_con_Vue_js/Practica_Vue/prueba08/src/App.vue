ffa7a3be1248390fe6b589558f9047825ef89f76
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-03-04 20:15:13 +0100

    Ejercicio completado

--- Tema_06_Desarrollo_con_Vue_js/Practica_Vue/prueba08/src/App.vue
+++ Tema_06_Desarrollo_con_Vue_js/Practica_Vue/prueba08/src/App.vue
@@ -1,18 +1,64 @@
 <script>
-
+import SeguidorGitHub from './components/SeguidorGitHub.vue';
 export default {
     name:"Principal",
     components:{
-
+        SeguidorGitHub
     },
     data(){
         return{
-            titulo:"Buscar un usuario de GitHub"
+            // Variables iniciales y de prueba
+            titulo:"Buscar un usuario de GitHub",
+
+            // Variables del display
+            primeraVez: true,
+            usuarioExistente: false,
+
+            // Variables para las URL y GitHub
+            urlBasica: "https://api.github.com/users/",
+            usuario: "",
+            urlCompleta: "",
+            seguidores: "",
+            login:"",
+            avatar_url:"",
+            html_url:""
+
         }
     },
     methods:{
         borrarConsola(){
             console.clear();
+        },
+        borrarInput(){
+            this.usuario="";
+        },
+        obtenerUsuario(){
+            this.primeraVez=false;
+            console.log("Hola "+this.usuario);
+
+            // fetch
+            this.urlCompleta = `${this.urlBasica}${this.usuario}/followers`
+            fetch(this.urlCompleta)
+            .then((response)=>{
+                if(response.status!=200){
+                    this.usuarioExistente = false;
+                    console.log(`Usuario inexistente: ${response.status}`);
+                    return;
+                } else {
+                    this.usuarioExistente = true;
+                    console.log(`El Usuario existe: ${response.status}`);
+                    return response.json();
+                    
+                }
+            })
+            .then((data)=>{
+                //console.log(data)
+                this.seguidores = data
+
+                //console.log(this.seguidores)
+            })
+            .catch((error)=>console.log("ERROR: " + error))
+            
         }
     }
 }
@@ -27,10 +73,38 @@ export default {
 
 
 <main class="container">
+    <div class="row">
+        <div class="col col-6">
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1"
+                v-model="usuario" @keydown.enter="obtenerUsuario"> 
+                <span class="input-group-text" id="basic-addon1">@</span>
+            </div>
+        </div>
+        <div class="col col-1">
+            <button class="btn btn-danger" @click="borrarConsola">#</button>
+        </div>
+        <div class="col col-1">
+            <button class="btn btn-success" @click="borrarInput">@</button> 
+        </div>
+    </div>
+    <div class="container" v-if="primeraVez==false">
+        <div class="row" v-if="usuarioExistente==false">
+            <div class="alert alert-danger" role="alert">
+                Usuario {{usuario}} inexistente.
+            </div>
+        </div>
 
-
+        <div class="row" v-else>
+            <div class="alert alert-primary" role="alert">
+                Usuario {{usuario}} existente.
+            </div>
+            <div v-for="unSeguidor in seguidores" :key="unSeguidor.id" class="col col-4">
+                <SeguidorGitHub :cadaSeguidor="unSeguidor"></SeguidorGitHub>
+            </div>
+        </div>
+    </div>
 </main>
-
 
 </template>
 
---
+++ Tema_06_Desarrollo_con_Vue_js/Practica_Vue/prueba08/src/components/SeguidorGitHub.vue
@@ -0,0 +1,47 @@
+<script>
+
+export default {
+    name:"Seguidores",
+    data(){
+        return{
+            // Variables iniciales y de prueba
+            titulo:"Seguidores de GitHub",
+
+        }
+    },
+    props:[
+        "cadaSeguidor"
+    ]
+}
+
+
+</script>
+
+<template>
+
+
+
+<main class="container">
+    <h1></h1>
+    <div class="card" style="width: 18rem;">
+  <img :src="cadaSeguidor.avatar_url" class="card-img-top" alt="cada seguidor">
+  <div class="card-body">
+    <h5 class="card-title">{{ cadaSeguidor.login }}</h5>
+    <a :href="cadaSeguidor.html_url" class="btn btn-primary">Ver perfil</a>
+  </div>
+</div>
+</main>
+
+
+</template>
+
+
+<style scoped>
+h1{
+    text-align: center;
+    color:rgb(232, 161, 29);
+    font-size:3em;
+    font-weight: bold;
+    text-decoration: underline;
+}
+</style>
