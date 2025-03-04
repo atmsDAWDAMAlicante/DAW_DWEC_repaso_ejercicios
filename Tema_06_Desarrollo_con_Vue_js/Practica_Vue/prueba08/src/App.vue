b941f948f96cbe03b9746ba06b305c166bf44429
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-03-04 19:32:09 +0100

    Primeros pasos en App.vue

--- Tema_06_Desarrollo_con_Vue_js/Practica_Vue/prueba08/src/App.vue
+++ Tema_06_Desarrollo_con_Vue_js/Practica_Vue/prueba08/src/App.vue
@@ -1,11 +1,46 @@
-<script >
+<script>
+
+export default {
+    name:"Principal",
+    components:{
+
+    },
+    data(){
+        return{
+            titulo:"Buscar un usuario de GitHub"
+        }
+    },
+    methods:{
+        borrarConsola(){
+            console.clear();
+        }
+    }
+}
+
 
 </script>
 
 <template>
-  
+  <header>
+    <h1>{{ titulo }}</h1>
+  </header>
+
+
+<main class="container">
+
+
+</main>
+
+
 </template>
 
-<style scoped>
 
+<style scoped>
+h1{
+    text-align: center;
+    color:rgb(232, 161, 29);
+    font-size:3em;
+    font-weight: bold;
+    text-decoration: underline;
+}
 </style>
